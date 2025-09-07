24015ad951074aea082d077d75f30e4c182f5624
Kanzah-Irshad <kanzahirshad@gmail.com>
2025-09-08 01:07:03 +0500

    changedlinks

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://edu-feedback-mern-frontend.vercel.app',
   credentials: true,
   exposedHeaders: ['Authorization', 'Set-Cookie'],
   allowedHeaders: ['Content-Type', 'Authorization']
